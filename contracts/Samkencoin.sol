384102dfeb662146f9f90a4e51ea9d8a690cc3a7
samkenxstream <100836531+samkenxstream@users.noreply.github.com>
2022-03-28 20:30:57 +0800

    Update Samkencoin.sol

--- contracts/Samkencoin.sol
+++ contracts/Samkencoin.sol
@@ -4,7 +4,7 @@ import { ERC20Pausable } from "openzeppelin-solidity/contracts/token/ERC20/ERC20
 import { ERC20Detailed } from "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 
-contract MaticToken is ERC20Pausable, ERC20Detailed {
+contract Samkencoin is ERC20Pausable, ERC20Detailed {
     constructor (string memory name, string memory symbol, uint8 decimals, uint256 totalSupply)
     public
     ERC20Detailed (name, symbol, decimals) {
